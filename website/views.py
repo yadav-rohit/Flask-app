4acfe40cc8cdb59c30f164d2e3fb05320371cefb
yadav-rohit <ry9878614394@gmail.com>
2024-07-27 18:08:27 +0530

    full structur

--- website/views.py
+++ website/views.py
@@ -7,11 +7,23 @@ import time
 import json
 import sqlite3
 from website import app
+from flask import render_template, session, jsonify, request, Response, redirect, url_for, send_from_directory
 
 # --Home Page --------------------------------#
 
-app.config.from_object(__name__)
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+# print("efwr", sys.getdefaultencoding())
+# app.config.from_object(__name__)
+
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Welcome to the Simple Flask App!'
+    return render_template('index.html')
+    try:
+        # Example data
+        return render_template('index.html')
+    except UnicodeDecodeError as e:
+        # Log the error for debugging
+        app.logger.error(f"UnicodeDecodeError: {e}")
+        return "There was an error rendering the template."
